7965950a4ff297d4614dafea1bb8b4a8ea09d924
EYE MOTION <eyemotiongamestudio@gmail.com>
2021-07-06 16:08:10 +0400

    upload

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
-const lotion = require('lotion');
+let lotion = require('lotion')
 
 let app = lotion({
-    initialState: {
-      count: 0
-    }
-  });
+  initialState: {
+    count: 0
+  }
+})
 
-  // Middleware MUST be deterministic
-  app.use( (state, tx) => {
-    if (state.count === tx.nonce) {
-        state.count++
-    }
-  })
+app.use(function(state, tx) {
+  if (state.count === tx.nonce) {
+    state.count++
+  }
+})
 
-  app.start()
+app.start()
+
+/*app.listen(3000)
+  .then(genesis => {
+    console.log('App listening on port 3000. You have launched your 1st blockchain!')
+  });*/
